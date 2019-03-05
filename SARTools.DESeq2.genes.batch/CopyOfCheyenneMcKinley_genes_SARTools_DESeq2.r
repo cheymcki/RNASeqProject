fddf613c4aaf1561b0034ab6065934a2290c1b35
Cheyenne McKinley <cnm006@bucknell.edu>
2019-03-05 15:48:55 -0500

    Ran analysis with genes.batch and transcripts

--- SARTools.DESeq2.genes.batch/CopyOfCheyenneMcKinley_genes_SARTools_DESeq2.r
+++ SARTools.DESeq2.genes.batch/CopyOfCheyenneMcKinley_genes_SARTools_DESeq2.r
@@ -10,18 +10,18 @@
 ################################################################################
 rm(list=ls())                                        # remove all the objects from the R session
 
-workDir <- "/Users/cnm006/Documents/RNASeqProject/SARTools.DESeq2.genes"      # working directory for the R session
+workDir <- "/Users/cnm006/Documents/RNASeqProject/SARTools.DESeq2.genes.batch"      # working directory for the R session
 
-projectName <- "SARTools.DESeq2.genes"                         # name of the project
+projectName <- "SARTools.DESeq2.genes.batch"                         # name of the project
 author <- "Cheyenne McKinley"                                # author of the statistical analysis/report
 
 targetFile <- "../genes.target.txt"                           # path to the design/target file
 rawDir <- "../SARTools"                                      # path to the directory containing raw counts files
-featuresToRemove <- NULL        # names of the features to be removed
+featuresToRemove <- "batch"        # names of the features to be removed
 
 varInt <- "Treatment"                                    # factor of interest
 condRef <- "Untreated"                                      # reference biological condition
-batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
+batch <- "batch"                                        # blocking factor: NULL (default) or "batch" for example
 
 idColumn = 1                                         # column with feature Ids (usually 1)
 countColumn = 5                                      # column with counts  (2 for htseq-count, 7 for featurecounts, 5 for RSEM/Salmon, 4 for kallisto)
--- SARTools.DESeq2.genes/CheyenneMcKinley_genes_SARTools_DESeq2.r
+++ SARTools.DESeq2.genes/CheyenneMcKinley_genes_SARTools_DESeq2.r
@@ -21,7 +21,7 @@ featuresToRemove <- NULL        # names of the features to be removed
 
 varInt <- "Treatment"                                    # factor of interest
 condRef <- "Untreated"                                      # reference biological condition
-batch <- "batch"                                        # blocking factor: NULL (default) or "batch" for example
+batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
 
 idColumn = 1                                         # column with feature Ids (usually 1)
 countColumn = 5                                      # column with counts  (2 for htseq-count, 7 for featurecounts, 5 for RSEM/Salmon, 4 for kallisto)
